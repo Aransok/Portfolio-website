b192eae14da7eaa3723b7fa72c9e763c6d26d10c
Aransok <113589022+Aransok@users.noreply.github.com>
2025-02-07 15:53:45 +0200

    Few more fixes now we left only to make the llm summarizer work

--- portfolio_website/urls.py
+++ portfolio_website/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
